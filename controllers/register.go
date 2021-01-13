a8f99cf8069e0e9630ae1ee975593592600af7f5
musJinx <musJinx10@outlook.com>
2021-01-13 18:07:15 +0800

    好像有哪里不对。。echo 长的跟 gin 有点不一样

---
+++ controllers/register.go
@@ -0,0 +1,6 @@
+package controllers
+
+import "github.com/labstack/echo"
+
+func Register(c echo.Context) error {
+}
--- server.go
+++ server.go
@@ -1,1 +1,27 @@
 package wallet
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"net/http"
+	"wallet/controllers"
+)
+
+func routerSet() {
+	e := echo.New()
+	// 跨域请求配置
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderAccept, echo.HeaderOrigin, echo.HeaderContentType},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST},
+		AllowCredentials: true,	// 允许 cookie
+		MaxAge: 43200})) // 预检结果能保留 12h
+	g := e.Group("/wallet")
+	{
+		g.POST("/register", controllers.Register)
+	}
+}
+
+func main() {
+\
+}
