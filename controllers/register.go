1623b29d4c5927b58357e4ad44ba6db0c748bc4a
musJinx <musJinx10@outlook.com>
2021-01-14 19:08:12 +0800

    初始化后端基本完成

--- controllers/register.go
+++ controllers/register.go
@@ -1,6 +1,59 @@
 package controllers
 
-import "github.com/labstack/echo"
+import (
+	"bytes"
+	"crypto/sha256"
+	"github.com/labstack/echo"
+	"io/ioutil"
+	"math/big"
+	"net/http"
+	"net/url"
+	"wallet/ELGamal"
+)
+const (
+	ErrorValue = "value cannot be blank"
+	RejectServer = "Server Error"
+)
+type newWallet struct {
+	name string `json:"name" form:"name"`
+	id 	 string `json:"id" form:"id"`
+	str  string `json:"str" form:"str"`
+}
 
 func Register(c echo.Context) error {
+	w := new(newWallet)
+	// 因为 echo 的 bind 无绑定检查功能
+	if err := c.Bind(w); err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	// 暂时只能验证是否为空
+	if w.id == "" || w.name == "" || w.str == "" {
+		return c.JSON(http.StatusBadRequest, ErrorValue)
+	}
+	// 计算公私钥
+	pub, priv, err := ELGamal.GenerateKeys(w.str)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	// 取哈希
+	pub.G1 = new(big.Int)
+	HashInfoBuf := sha256.Sum256([]byte(w.str))
+	// 向监管者提交注册请求，并返回相关信息
+	// [32]byte 是一个数组，要把他转换成切片
+	if resp, err := http.PostForm("http://39.106.173.191：1423/register", url.Values{"name": {w.name}, "id": {w.id}, "Hashky": {string(HashInfoBuf[:])}}); err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return c.JSON(http.StatusInternalServerError, err)
+	} else {
+		if res, err := ioutil.ReadAll(resp.Body); err != nil {
+			c.JSON(http.StatusInternalServerError, err)
+			return c.JSON(http.StatusInternalServerError, err)
+		} else {
+			// 判断应该返回的信息
+			if bytes.Equal(res,[]byte("Successful!")) {
+				return c.JSON(http.StatusOK, priv)
+			} else {
+				return c.JSON(http.StatusInternalServerError, RejectServer)
+			}
+		}
+	}
 }
--- server.go
+++ server.go
@@ -1,13 +1,16 @@
 package wallet
 
 import (
+	"context"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"net/http"
+	"os"
+	"os/signal"
+	"time"
 	"wallet/controllers"
 )
 
-func routerSet() {
+func main() {
 	e := echo.New()
 	// 跨域请求配置
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
@@ -16,12 +19,27 @@ func routerSet() {
 		AllowMethods: []string{echo.GET, echo.PUT, echo.POST},
 		AllowCredentials: true,	// 允许 cookie
 		MaxAge: 43200})) // 预检结果能保留 12h
+	// 一组路由
 	g := e.Group("/wallet")
 	{
 		g.POST("/register", controllers.Register)
 	}
-}
-
-func main() {
-\
+	// 网页的静态文件
+	e.File("/", "html/main.html")
+	// 启动服务，平滑关闭
+	go func() {
+		if err := e.Start(":80"); err != nil{
+			e.Logger.Fatal("Fail to star with error:%v", err)
+		}
+	}()
+	// 监听停止信号
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	// 留 5s 处理已经接受的请求，然后关闭服务
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil{
+		e.Logger.Fatal("Fail to shutdown with error", err)
+	}
 }
